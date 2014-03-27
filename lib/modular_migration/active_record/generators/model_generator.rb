d834b3ee946e7f5f7c8ce980057721227c2ad6f7
lijia.tong <wtuyuupe@163.com>
2014-03-27 20:57:54 +0800

    Rewrites create_migration_file

--- lib/modular_migration/active_record/generators/model_generator.rb
+++ lib/modular_migration/active_record/generators/model_generator.rb
@@ -2,7 +2,12 @@ module ActiveRecord
   module Generators # :nodoc:
     class ModelGenerator < Base # :nodoc:
 
-      def create_modular_migration_file
+      class_option :migration,  type: :boolean, default: true
+      class_option :timestamps, type: :boolean, default: true
+
+      def create_migration_file
+        return unless options[:migration] && options[:parent].nil?
+        attributes.each { |a| a.attr_options.delete(:index) if a.reference? && !a.has_index? } if options[:indexes] == false
         template_file = Rails.version.to_i < 4 ? 'migration.rb' : '../../migration/templates/create_table_migration.rb'
         migration_file = File.join(FileUtils.mkdir_p(File.join('db/migrate', class_path.join('/'), file_name)), "/create_#{table_name}.rb")
         migration_template template_file, migration_file
